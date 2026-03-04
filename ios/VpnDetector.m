8b4fec34a6a8e03b26f509301a73c8eb04443252
maksim k <fya-07@list.ru>
2026-03-05 00:20:06 +0700

    add .m

---
+++ ios/VpnDetector.m
@@ -0,0 +1,8 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(VpnDetector, NSObject)
+
+RCT_EXTERN_METHOD(isVpnActive:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+@end
